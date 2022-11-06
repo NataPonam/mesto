30669440f5b5b2ee2098b0476e29cd43745f2434
Наталья Понамаренко <nuts08air@inbox.ru>
2022-11-06 22:25:51 +0800

    Проверка_2

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,33 @@
-
-let popupOpen = document.querySelector('.profile__btn-edit');
 let popupElem = document.querySelector('.popup');
+let popupOpen = document.querySelector('.profile__btn-edit');
 let popupClose = popupElem.querySelector('.popup__close-icon');
 let profileName = document.querySelector('.profile__name');
 let profileInfo = document.querySelector('.profile__info');
 
 let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__name');
-let jobInput = document.querySelector('.popup__info');
+let nameInput = document.querySelector('.popup__input_name');
+let jobInput = document.querySelector('.popup__input_info');
 
-popupOpen.addEventListener('click', function () {
+function openPopup() {
   popupElem.classList.add('popup_opened');
   nameInput.value = profileName.textContent;
   jobInput.value = profileInfo.textContent;
-});
+};
 
-popupClose.addEventListener('click', function () {
+function closePopup() {
   popupElem.classList.remove('popup_opened');
-});
+};
+
+popupOpen.addEventListener('click', openPopup);
+popupClose.addEventListener('click', closePopup);
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileInfo.textContent = jobInput.value;
-}
+  closePopup();
+};
+
 formElement.addEventListener('submit', formSubmitHandler);
 
 
