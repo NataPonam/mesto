82d5847ec0ad0f074252c7e84c9929de12fb6b8e
Наталья Понамаренко <nuts08air@inbox.ru>
2022-11-17 23:48:32 +0800

    css

--- scripts/index.js
+++ scripts/index.js
@@ -129,7 +129,7 @@ const generateCard = (cardInfo) => {
 
   const bigPicElement = document.querySelector('.popup_img');
   const imgBig = document.querySelector('.popup__big-pic');
-  const titlePic = document.querySelector('.popup_img_title');
+  const titlePic = document.querySelector('.popup__pic_title');
   const closePopupImg = document.querySelector('.popup__close-icon_big-pic');
 
   function handleBigSizePic() {
