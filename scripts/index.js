d96a9c8521270421c2dc447b98c546217844b71b
Наталья Понамаренко <nuts08air@inbox.ru>
2022-11-20 01:03:18 +0800

    Ревью_2

--- scripts/index.js
+++ scripts/index.js
@@ -1,95 +1,72 @@
-let popupElem = document.querySelector('.popup_profile');
-let popupOpen = document.querySelector('.profile__btn-edit');
-let popupClose = popupElem.querySelector('.popup__close-icon');
-let profileName = document.querySelector('.profile__name');
-let profileInfo = document.querySelector('.profile__info');
+const popup = document.querySelector('.popup')
 
-let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__input_type_name');
-let jobInput = document.querySelector('.popup__input_type_info');
+const popupElemProfile = document.querySelector('.popup_profile');
+const popupOpen = document.querySelector('.profile__btn-edit');
+const popupClose = popupElemProfile.querySelector('.popup__close-icon');
+const profileName = document.querySelector('.profile__name');
+const profileInfo = document.querySelector('.profile__info');
 
-function openPopup() {
-  popupElem.classList.add('popup_opened');
+const formElement = document.querySelector('.popup__form');
+const nameInput = document.querySelector('.popup__input_type_name');
+const jobInput = document.querySelector('.popup__input_type_info');
+
+const popupElementPlaces = document.querySelector('.popup_places');
+const popupPlaceOpen = document.querySelector('.profile__btn-add');
+const popupPlaceClose = popupElementPlaces.querySelector('.popup__close-icon');
+
+const cardsList = document.querySelector('.cards__list'); //СПИСОК ВСЕХ КАРТОЧЕК//
+const formPlaces = document.querySelector('.popup__form-places');//ФОРМА//
+const placeInput = document.querySelector('.popup__input_type_place'); //ИНПУТ МЕСТО//
+const linkImgInput = document.querySelector('.popup__input_type_link'); //ИНПУТ ССЫЛКА//
+
+const popupElementImages = document.querySelector('.popup_img');
+const popupPicture = popupElementImages.querySelector('.popup__big-pic');
+const pictureFigcaption = popupElementImages.querySelector('.popup__figcaption');
+const closePopupImg = document.querySelector('.popup__close-icon_big-pic');
+
+function openPopup(popup) {
+  popup.classList.add('popup_opened');///ОБЩАЯ ФУНКЦИЯ ОТКРЫТИЯ/
+};
+
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');//ОБЩАЯ ФУНКЦИЯ ЗАКРЫТИЯ//
+};
+
+//ПОПАП профиль//
+popupOpen.addEventListener('click', function () {
+  openPopup(popupElemProfile);
   nameInput.value = profileName.textContent;
   jobInput.value = profileInfo.textContent;
-};
+});
 
-function closePopup() {
-  popupElem.classList.remove('popup_opened');
-};
-
-popupOpen.addEventListener('click', openPopup);
-popupClose.addEventListener('click', closePopup);
+popupClose.addEventListener('click', function () {
+  closePopup(popupElemProfile);
+});
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileInfo.textContent = jobInput.value;
-  closePopup();
+  closePopup(popupElemProfile);
 };
 
 formElement.addEventListener('submit', formSubmitHandler);
 
-//Попап добавления локации//
+//ПОПАП добавления локации//
+popupPlaceOpen.addEventListener('click', function () {
+  openPopup(popupElementPlaces);
+});
 
-let popupEl = document.querySelector('.popup_places');
-let popupPlaceOpen = document.querySelector('.profile__btn-add');
-let popupPlaceClose = popupEl.querySelector('.popup__close-icon');
-
-
-function openPopupPlaces() {
-  popupEl.classList.toggle('popup_places_opened');
-
-};
-function closePopupPlaces() {
-  popupEl.classList.remove('popup_places_opened');
-};
-
-popupPlaceOpen.addEventListener('click', openPopupPlaces);
-popupPlaceClose.addEventListener('click', closePopupPlaces);
-
-//Карточки//
-
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
-
-//Переменные//
-
-const cardsBox = document.querySelector('.cards__list'); //СПИСОК ВСЕХ КАРТОЧЕК//
-const formPlaces = document.querySelector('.popup__form-places');//ФОРМА//
-const place = document.querySelector('.popup__input_type_place'); //ИНПУТ МЕСТО//
-const direction = document.querySelector('.popup__input_type_link'); //ИНПУТ ССЫЛКА//
+popupPlaceClose.addEventListener('click', function () {
+  closePopup(popupElementPlaces);
+});
 
 const handleSubmitAddPlace = (event) => {
   event.preventDefault();
-  renderCards({ name: place.value, link: direction.value });
-  place.value = ' '; //очистить поля//
-  direction.value = ' ';
-  closePopupPlaces();//закрыть попап по кнопке сохранить//
+  renderInitialCards({ name: placeInput.value, link: linkImgInput.value });
+  placeInput.value = '';
+  linkImgInput.value = '';
+  closePopup(popupElementPlaces);
 }
 
 formPlaces.addEventListener('submit', handleSubmitAddPlace);
@@ -98,24 +75,22 @@ formPlaces.addEventListener('submit', handleSubmitAddPlace);
 const cardTemplate = document.querySelector('#cards__template').content.querySelector('.card');
 
 //Удаление карточки//
-
 const handleDeleteCard = (event) => {
   event.target.closest('.card').remove();
 }
 
 //Генерация карточки//
-
 const generateCard = (cardInfo) => {
 
   const cardClone = cardTemplate.cloneNode(true); //Клон из темплейта, работаем с ним//
-  const title = cardClone.querySelector('.card__title');
-  title.textContent = cardInfo.name;
+  const cardtitle = cardClone.querySelector('.card__title');
+  cardtitle.textContent = cardInfo.name;
 
-  const image = cardClone.querySelector('.card__img');
-  image.src = cardInfo.link;
+  const cardImage = cardClone.querySelector('.card__img');
+  cardImage.src = cardInfo.link;
 
-  const deleteBtn = cardClone.querySelector('.card__trash');
-  deleteBtn.addEventListener('click', handleDeleteCard);
+  const deleteCardBtn = cardClone.querySelector('.card__trash');
+  deleteCardBtn.addEventListener('click', handleDeleteCard);
 
   //Лайк карточки//
 
@@ -127,36 +102,27 @@ const generateCard = (cardInfo) => {
 
   //Увеличение картинки//
 
-  const bigPicElement = document.querySelector('.popup_img');
-  const imgBig = document.querySelector('.popup__big-pic');
-  const titlePic = document.querySelector('.popup__figcaption');
-  const closePopupImg = document.querySelector('.popup__close-icon_big-pic');
-
-  function handleBigSizePic() {
-    bigPicElement.classList.toggle('card__img_zoom');
-    imgBig.src = image.src;
-    titlePic.textContent = title.textContent;
+  function handleZoomImage() {
+    openPopup(popupElementImages);
+    popupPicture.src = cardImage.src;
+    pictureFigcaption.textContent = cardtitle.textContent;
   }
-  image.addEventListener('click', handleBigSizePic); //Повесили клик по картинке//
+  cardImage.addEventListener('click', handleZoomImage); //Повесили клик по картинке//
 
-  //Закрытие попапа с изображением//
-  function handleClosePopupImg() {
-    bigPicElement.classList.remove('card__img_zoom');
-  }
-  closePopupImg.addEventListener('click', handleClosePopupImg);
+  closePopupImg.addEventListener('click', function () {
+    closePopup(popupElementImages);
+  });
 
   return cardClone;
 }
 
 //Добавление карточки//
-
-const renderCards = (cardInfo) => {
-  cardsBox.prepend(generateCard(cardInfo));
+const renderInitialCards = (cardInfo) => {
+  cardsList.prepend(generateCard(cardInfo));
 };
 
 //Рендер для карточек//
-
 initialCards.forEach((cardInfo) => {
-  renderCards(cardInfo);
+  renderInitialCards(cardInfo);
 });
 
